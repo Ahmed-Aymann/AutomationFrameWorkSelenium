353bf73f950e464552f62a0ca7c7fc064afb552c
merna-alaa <mernaalaa377@gmail.com>
2024-12-15 12:15:35 +0200

    12 done

--- src/test/java/PageObjects/AddProductsinCart/AddProductActions.java
+++ src/test/java/PageObjects/AddProductsinCart/AddProductActions.java
@@ -1,4 +1,54 @@
 package PageObjects.AddProductsinCart;
 
-public class AddProductActions {
+import Assertions.Assertion;
+import BrowserActions.BrowserActions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AddProductActions extends AddProductElements{
+    BrowserActions browserActions;
+    Assertion assertion;
+    public AddProductActions(WebDriver driver) {
+        browserActions = new BrowserActions(driver);
+        assertion = new Assertion(driver);
+    }
+    public void navigateToUrl(String url){
+        browserActions.navigateToURl(url);
+    }
+    public boolean isHomePageVisible() {
+        return browserActions.waitUntilElementIsReady(homePageVisibleElement).isDisplayed();
+    }
+
+    public void clickProductsButton() {
+        browserActions.click(productsButton);
+    }
+    public void hoverAndAddFirstProductToCart() {
+        browserActions.waitUntilElementIsReady(FirstProduct).click();
+        browserActions.click(addToCartFirstProduct);
+    }
+    public void clickContinueShopping() {
+        browserActions.click(continueShoppingButton);
+    }
+    public void hoverAndAddSecondProductToCart() {
+        browserActions.waitUntilElementIsReady(SecondProduct).click();
+        browserActions.click(addToCartSecondProduct);
+    }
+    public void clickViewCartButton() {
+        browserActions.click(viewCartButton);
+    }
+    public boolean areBothProductsAdded() {
+        return browserActions.waitUntilElementIsReady(firstProductInCart).isDisplayed()
+                && browserActions.waitUntilElementIsReady(secondProductInCart).isDisplayed();
+    }
+    public String getProductPrice(By priceLocator) {
+        return browserActions.getText(priceLocator);
+    }
+    public String getProductQuantity(By quantityLocator) {
+        return browserActions.getText(quantityLocator);
+    }
+    public String getProductTotal(By totalLocator) {
+        return browserActions.getText(totalLocator);
+    }
+
 }
--- src/test/java/PageObjects/VerifyProductquantityinCart/VerifyProductElements.java
+++ src/test/java/PageObjects/VerifyProductquantityinCart/VerifyProductElements.java
@@ -2,3 +2,4 @@ package PageObjects.VerifyProductquantityinCart;
 
 public class VerifyProductElements {
 }
+
