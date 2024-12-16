3a6d0c7e72a9aad25c784e60b587cebddbf5a614
ranawalid <rorom1512001@gmail.com>
2024-12-16 10:24:52 +0200

    testcase18 is done

--- src/test/java/PageObjects/NavBar/NavBarElements.java
+++ src/test/java/PageObjects/NavBar/NavBarElements.java
@@ -4,4 +4,9 @@ import org.openqa.selenium.By;
 
 public class NavBarElements {
     By SignUpLoginButton = By.xpath("//a[@href='/login']");
+    By ProductButton = By.xpath("//a[@href='/products']");
+
+
+
+
 }
